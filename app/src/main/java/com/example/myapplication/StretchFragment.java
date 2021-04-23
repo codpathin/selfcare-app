7437f24798a33d74d6593aedffbe5ea718b6b56b
Lawrence Nguyen <chocoballss123@gmail.com>
2021-04-22 17:54:55 -0700

    Added Stretch Fragment

---
+++ app/src/main/java/com/example/myapplication/StretchFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StretchFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StretchFragment extends Fragment {
+
+
+    public StretchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stretch, container, false);
+    }
+}
