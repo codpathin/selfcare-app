b7038d92fe16a8c5ab861e8805208e4c72a2ca71
otterice <josephdang101@gmail.com>
2021-04-29 19:15:16 -0700

    persistance on calendar with notes
    rewriting date notes does not work

--- app/src/main/java/com/example/myapplication/MyEventDay.java
+++ app/src/main/java/com/example/myapplication/MyEventDay.java
@@ -9,42 +9,35 @@ import java.util.Calendar;
 
 public class MyEventDay extends EventDay implements Parcelable {
     private String mNote;
-
-    MyEventDay(Calendar day, int imageResource, String note) {
+    public MyEventDay(Calendar day, int imageResource, String note) {
         super(day, imageResource);
         mNote = note;
     }
-
-    String getNote() {
+    public  String getNote() {
         return mNote;
     }
-
     private MyEventDay(Parcel in) {
         super((Calendar) in.readSerializable(), in.readInt());
         mNote = in.readString();
     }
-
-    public static final Parcelable.Creator<MyEventDay> CREATOR = new Parcelable.Creator<MyEventDay>() {
+    public static final Creator<MyEventDay> CREATOR = new Creator<MyEventDay>() {
         @Override
-        public MyEventDay createFromParcel(Parcel source) {
-            return new MyEventDay(source);
+        public MyEventDay createFromParcel(Parcel in) {
+            return new MyEventDay(in);
         }
-
         @Override
         public MyEventDay[] newArray(int size) {
             return new MyEventDay[size];
         }
     };
-
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeSerializable(getCalendar());
+        parcel.writeInt(getImageResource());
+        parcel.writeString(mNote);
+    }
     @Override
     public int describeContents() {
         return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeSerializable(getCalendar());
-        dest.writeInt(getImageResource());
-        dest.writeString(mNote);
     }
 }
