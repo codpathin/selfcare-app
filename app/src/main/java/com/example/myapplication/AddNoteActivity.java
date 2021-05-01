868a3401936ed8f00819c42284097dcb043e4d76
viviantnguyen <09vnguyen02@gmail.com>
2021-04-30 21:01:27 -0700

    renamed add_note_activity.xml -> activity_addnote.xml, adjusted layout

--- app/src/main/java/com/example/myapplication/AddNoteActivity.java
+++ app/src/main/java/com/example/myapplication/AddNoteActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +14,11 @@ import com.applandeo.materialcalendarview.CalendarView;
 import com.example.myapplication.fragments.CalendarFragment;
 
 public class AddNoteActivity extends AppCompatActivity {
+    MyEventDay myEventDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_note_activity);
+        setContentView(R.layout.activity_addnote);
 
         final CalendarView datePicker = (CalendarView) findViewById(R.id.datePicker);
         Button button = (Button) findViewById(R.id.addNoteButton);
@@ -26,10 +28,12 @@ public class AddNoteActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent returnIntent = new Intent();
-
-                MyEventDay myEventDay = new MyEventDay(datePicker.getSelectedDate(),
-                        R.drawable.ic_message_black_48dp, noteEditText.getText().toString());
-
+                if (!(myEventDay instanceof MyEventDay)) {
+                    myEventDay = new MyEventDay(datePicker.getSelectedDate(),
+                            R.drawable.ic_message_black_48dp, noteEditText.getText().toString());
+                } else {
+                    myEventDay.setNote(noteEditText.getText().toString());
+                }
                 returnIntent.putExtra(CalendarFragment.RESULT, myEventDay);
                 setResult(Activity.RESULT_OK, returnIntent);
                 finish();
